27cc56e8ef03a1ae40ca5a19178d5dcb2bfc55a2
Jhon Achata Limachi <jhonachata.dev@gmail.com>
2025-02-09 11:42:22 -0500

    Update sponsors

--- src/views/SponsorsView.vue
+++ src/views/SponsorsView.vue
@@ -1,10 +1,25 @@
+<script setup>
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
+
+  const route = useRoute();
+  const b = computed(() => route.query.b || 0);
+</script>
 <template>
   <div class="overlay">
     <div class="header">
       <!-- <h1>Tu Nombre del Stream</h1> -->
     </div>
+<<<<<<< Updated upstream
     <div class="footer">
       <video src="@/assets/video/sponsors.webm" autoplay loop></video>
+=======
+    <div class="footer" :style="`bottom: ${b}px;`">
+      <video autoplay loop>
+        <source src="@/assets/video/sponsors.webm" type="video/mp4">
+        Tu navegador no soporta el elemento de video.
+      </video>
+>>>>>>> Stashed changes
     </div>
   </div>
 </template>
@@ -41,7 +56,6 @@ body {
 
 .footer {
   position: absolute;
-  bottom: 0;
   left: 50%;
   transform: translateX(-50%);
   /* background: rgba(0, 0, 0, 0.5); */
